72670a9c18c776f19d9486496eb6b5fa088b02e2
NumeroUno <numerounodeveloper@gmail.com>
2025-04-18 10:28:02 -0400

    deleted some not required files

--- test_del.py
+++
@@ -1,9 +0,0 @@
-import aeneas
-
-print(aeneas.__version__) # Print the version
-
-try:
-    config = aeneas.Config(audio_file_path="audio.wav", text_file_path="normalized_text.txt", language="en")
-    print("Aeneas Config created successfully!")
-except Exception as e:
-    print(f"Error: {e}")
